4f34fa0308c55a2a12db977d3c85e483229ed1e2
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-10-27 13:03:30 -0500

    Added cd functionality

--- app/main.py
+++ app/main.py
@@ -24,6 +24,13 @@ def exit(number):
 def pwd():
     print(os.getcwd())
 
+def cd(dir):
+    dir = ' '.join(dir)
+    if os.path.exists(dir):
+        os.chdir(dir)
+    else:
+        print("cd: {}: No such file or directory".format(dir))
+
 def type(cmd):
     cmd = ' '.join(cmd)
     if cmd in commands:
@@ -34,10 +41,10 @@ def type(cmd):
     else:
         print('{}: not found'.format(cmd))
 
-commands = {"exit": exit, "echo": echo, "type": type, "pwd": pwd}
+commands = {"exit": exit, "echo": echo, "type": type, "pwd": pwd, "cd": cd}
 def main():
     while (True):
-        
+
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
@@ -47,16 +54,18 @@ def main():
         # List of commands
         
 
-        # splitting the whole command into the main commands and its flags
+        # splitting the whole command into the main command and its flags
         split_command = user.split()
 
         if split_command[0] in commands and len(split_command) > 1:
             commands.get(split_command[0])(split_command[1:])
         elif split_command[0] in commands and len(split_command) == 1:
             commands.get(split_command[0])()
-        elif exists:=pathExists(split_command[0]):
+        elif (exists:=pathExists(split_command[0])) and len(split_command) > 1:
             args = ' '.join(split_command[1:])
             subprocess.run([exists, args])
+        elif (exists:=pathExists(split_command[0])) and len(split_command) == 1:
+            subprocess.run(exists)
         else:
             #print('{}: command not found'.format(user))
             notFound(split_command[0])
